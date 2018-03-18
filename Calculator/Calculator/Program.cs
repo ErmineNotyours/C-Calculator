41da8b3eb93c1fcd679af07f0ad4a28da8577972
ErmineNotyours <arthurallen@centurylink.net>
2018-03-18 10:25:16 -0700

    C sharp calculator project

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* *
+ * C# Calculator
+ * By Arthur Allen
+ * Using menu structure code from the Microsoft Virtual Adademy While Iteration Statement section
+ * */
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Persist the main menu
+
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+
+            MainMenu();
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Addition");
+            Console.WriteLine("2) Subtraction");
+            Console.WriteLine("3) Multiplication");
+            Console.WriteLine("4) Division");
+            Console.WriteLine("q) Quit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                Addition();
+                return true;
+            }
+            else if (result == "2")
+            {
+                Subtraction();
+                return true;
+            }
+            else if (result == "3")
+            {
+                Multiplication();
+                return true;
+            }
+            else if (result == "4")
+            {
+                Division();
+                return true;
+            }
+            else if ((result == "q") || (result == "Q"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void Addition()
+        {
+            Console.Clear();
+            Console.WriteLine("Addition");
+            Console.Write("Type a number to add: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            Console.Write("Type the next number to add: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+            decimal result = x + y;
+            Console.Write("{0} + {1} = {2} ", x, y, result);
+            Console.ReadLine();
+        }
+
+        private static void Subtraction()
+        {
+            Console.Clear();
+            Console.WriteLine("Subtraction");
+            Console.Write("Type a number to subtract: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            Console.Write("Type the next number to subtract: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+            decimal result = x - y;
+            Console.Write("{0} - {1} = {2} ", x, y, result);
+            Console.ReadLine();
+        }
+
+        private static void Multiplication()
+        {
+            Console.Clear();
+            Console.WriteLine("Multiplication");
+            Console.Write("Type a number to multiply: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            Console.Write("Type the next number to multiply: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+            decimal result = x * y;
+            Console.Write("{0} * {1} = {2} ", x, y, result);
+            Console.ReadLine();
+        }
+
+        private static void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Division");
+            Console.Write("Type a number to divide: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            Console.Write("Type the next number to divide: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+            if (y != 0)  // Check for division by zero
+            {
+                decimal result = x / y;
+                Console.Write("{0} / {1} = {2} ", x, y, result);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("You can't divide by zero! ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# C-Calculator
+# C# Calculator
+
+## How it was made
+
+In setting up this project, I thought of the menu structure in the _Microsoft Virtual Academy_ While Iterations section.  From there it was easy to setup methods to perform each major calculating functions: addition, subtraction, multiplication and division.  I added in a check to prevent division by zero, but everything else was straightforward asking for input and making simple calculations.  The variables are set to the decimal notation per the recommendation in _C# In a Nutshell_.  So far work on the project is taking two and a half hours.
+
+## Bugs
+
+Because I still don't understand the environment, typing "q" and enter does not quit the program right away, but requires a "q" and a double return.  Also, not entering a number and pressing return during number entry in the math functions results in a run-time error.
+
+## How to use it
+
+The user is presented with a menu option for four calculating functions: Addition, Subtraction, Multiplication and Division.  These can be chosen by entering numbers 1 through 4.  Additionally, the user may quit the program with "q" or "Q".  When one of the options is chosen, the user is prompted to enter one number, then enter another number and the result of the numbers processed by the mathematical operation is displayed.  Pressing enter one more time returns control to the main menu.
